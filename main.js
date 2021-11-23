11f6f34fa69f32746dcbd3b6413fcd5ba1c9c1fa
Maryam Tohidiyan <m.tohidiyan78@gmail.com>
2021-11-23 12:00:17 -0800

    finished

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const digital = document.querySelector(".digital")
+const hourHand =document.querySelector(".Hhand")
+ const minuteHand =document.querySelector(".Mhand")
+ const secondHand = document.querySelector(".Shand")
+ 
+
+
 setInterval(clockFunction , 1000 );
 function clockFunction(){
     const date = new Date();
@@ -10,26 +16,36 @@ function clockFunction(){
     if (hour === 0){
         hour = 12 ;
     }
-
     if(hour>12){
         hour = hour -12
         time = "PM"
     }
-
     if(hour<10){
         hour = `0${hour}`
     }
     if(minute<10){
         minute = `0${minute}`
     }
-
     if(second<10){
         second = `0${second}`
     }
-
     digital.innerText = `${hour}:${minute}:${second} ${time}`
 
-}
+    
 
+    let hr = date.getHours() * 30 ;
+    let mn = date.getMinutes() * 6 ; 
+    let sc = date.getSeconds() * 6 ;
+    let Hr = hr + (mn/12) ;
+    
+    secondHand.style.transform = `rotate(${sc}deg)`
+    minuteHand.style.transform = `rotate(${mn}deg)`
+    hourHand.style.transform = `rotate(${Hr}deg)`
+   
+
+
+
+
+}
 
 
